e9d886866aa40bf20a718aa79741fef783d6d138
yuxiang660 <yuxiang660@163.com>
2020-05-28 12:55:55 +0800

    add UVM notes

---
+++ UVMTutorial.md
@@ -0,0 +1,6 @@
+# 概述
+本文是[UVM Tutorial](https://www.chipverify.com/uvm/uvm-tutorial)的学习笔记。
+
+# UVM TestBench 和 SystemVerilog TestBench 对比
+![UVM](./code/uvm-tb.gif)
+
--- code/uvm-class/hello.sv
+++ code/uvm-class/hello.sv
@@ -3,6 +3,7 @@
 import uvm_pkg::*;
 
 class my_test extends uvm_test;
+    // Register my_test component with the UVM factory
     `uvm_component_utils (my_test)
 
     function new (string name = "my_test", uvm_component parent);
